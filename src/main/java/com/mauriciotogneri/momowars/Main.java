5bb4936ce6eb684d2054e11ca84958ebb138512c
mtogneri <mtogneri@openwt.com>
2017-11-19 18:28:15 +0100

    Removed HTTPS handler

--- src/main/java/com/mauriciotogneri/momowars/Main.java
+++ src/main/java/com/mauriciotogneri/momowars/Main.java
@@ -2,19 +2,11 @@ package com.mauriciotogneri.momowars;
 
 import com.mauriciotogneri.jerry.Jerry;
 
-import org.eclipse.jetty.server.Server;
-
 public class Main extends Jerry
 {
     public static void main(String[] args) throws Exception
     {
-        //        Main main = new Main();
-        //        main.start(8080, "com.mauriciotogneri.momowars");
-
-        Server server = new Server(8080);
-        server.setHandler(new HelloHandler());
-
-        server.start();
-        server.join();
+        Main main = new Main();
+        main.start(8080, "com.mauriciotogneri.momowars");
     }
 }
