62958edaea015e1f09263f8581f23bfb0c8bdd6a
mtogneri <mtogneri@openwt.com>
2017-11-19 18:46:09 +0100

    Testing HTTPS

--- src/main/java/com/mauriciotogneri/momowars/Main.java
+++ src/main/java/com/mauriciotogneri/momowars/Main.java
@@ -5,34 +5,41 @@ import com.mauriciotogneri.jerry.Jerry;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.SslConnectionFactory;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.servlet.ServletContainer;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class Main extends Jerry
 {
     public static void main(String[] args) throws Exception
     {
         Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
-        connector.setPort(9999);
-        HttpConfiguration https = new HttpConfiguration();
-        https.addCustomizer(new SecureRequestCustomizer());
-        ServerConnector sslConnector = new ServerConnector(server,
-                                                           new SslConnectionFactory("http/1.1"),
-                                                           new HttpConnectionFactory(https));
-        sslConnector.setPort(9998);
-        server.setConnectors(new Connector[] {connector, sslConnector});
 
-        ServletContextHandler context = new ServletContextHandler(1);
-        context.setContextPath("/");
-        server.setHandler(context);
-        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
-        jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "com.mauriciotogneri.momowars");
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(8443);
+
+        ServerConnector httpsConnector = new ServerConnector(server,
+                                                             new SslConnectionFactory("http/1.1"),
+                                                             new HttpConnectionFactory(http_config));
+        httpsConnector.setPort(8443);
+
+        server.setConnectors(new Connector[] {httpsConnector});
+
+        //ServletContextHandler context = new ServletContextHandler(1);
+        //context.setContextPath("/");
+        //server.setHandler(context);
+        server.setHandler(new HelloHandler());
+        //ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/*");
+        //jerseyServlet.setInitParameter("jersey.config.server.provider.packages", "com.mauriciotogneri.momowars");
 
         try
         {
@@ -44,8 +51,18 @@ public class Main extends Jerry
             server.destroy();
         }
 
-
         //Main main = new Main();
         //main.start(8080, "com.mauriciotogneri.momowars");
+    }
+
+    public static class HelloHandler extends AbstractHandler
+    {
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+        {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            baseRequest.setHandled(true);
+            response.getWriter().println("<h1>Hello World</h1>");
+        }
     }
 }
