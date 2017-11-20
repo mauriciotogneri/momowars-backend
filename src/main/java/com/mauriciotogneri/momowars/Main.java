2151d9f3971b93c7e373a0c747a7f81571f32623
mtogneri <mtogneri@openwt.com>
2017-11-20 21:35:48 +0100

    Test sending email

--- src/main/java/com/mauriciotogneri/momowars/Main.java
+++ src/main/java/com/mauriciotogneri/momowars/Main.java
@@ -1,11 +1,19 @@
 package com.mauriciotogneri.momowars;
 
 import com.mauriciotogneri.jerry.Jerry;
+import com.mauriciotogneri.momowars.email.Email;
 
 public class Main extends Jerry
 {
     public static void main(String[] args) throws Exception
     {
+        Email email = new Email(
+                "maxpawer@gmail.com",
+                "mauricio.togneri@gmail.com",
+                "Test",
+                "Working!");
+        email.send();
+
         Main main = new Main();
         main.start(Integer.valueOf(System.getenv("PORT")), "com.mauriciotogneri.momowars");
     }
