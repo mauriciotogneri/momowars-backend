06db8f03b335bee3dc29c85e8b609a4280ece835
mtogneri <mtogneri@openwt.com>
2017-11-24 21:26:39 +0100

    Logging requests and responses

--- src/main/java/com/mauriciotogneri/momowars/endpoint/ServerRequestFilter.java
+++ src/main/java/com/mauriciotogneri/momowars/endpoint/ServerRequestFilter.java
@@ -1,5 +1,12 @@
 package com.mauriciotogneri.momowars.endpoint;
 
+import com.mauriciotogneri.jerry.logging.RequestLogger;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
@@ -10,8 +17,32 @@ import javax.ws.rs.ext.Provider;
 public class ServerRequestFilter implements ContainerRequestFilter
 {
     @Override
-    public void filter(ContainerRequestContext requestContext)
+    public void filter(ContainerRequestContext request)
     {
-        System.out.println();
+        logRequest(request);
+    }
+
+    private void logRequest(ContainerRequestContext request)
+    {
+        try
+        {
+            StringBuilder builder = new StringBuilder();
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            builder.append(String.format(
+                    "<<< %s%n%n",
+                    dateFormat.format(new Date())));
+
+            builder.append(RequestLogger.log(request));
+
+            System.out.print(builder.toString());
+            System.out.flush();
+        }
+        catch (Exception e)
+        {
+            // ignore
+        }
     }
 }
--- src/main/java/com/mauriciotogneri/momowars/endpoint/ServerResponseFilter.java
+++ src/main/java/com/mauriciotogneri/momowars/endpoint/ServerResponseFilter.java
@@ -1,5 +1,12 @@
 package com.mauriciotogneri.momowars.endpoint;
 
+import com.mauriciotogneri.jerry.logging.ResponseLogger;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
@@ -11,8 +18,32 @@ import javax.ws.rs.ext.Provider;
 public class ServerResponseFilter implements ContainerResponseFilter
 {
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+    public void filter(ContainerRequestContext request, ContainerResponseContext response)
     {
-        System.out.println();
+        logResponse(response);
+    }
+
+    private void logResponse(ContainerResponseContext response)
+    {
+        try
+        {
+            StringBuilder builder = new StringBuilder();
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            builder.append(String.format(
+                    ">>> %s%n%n",
+                    dateFormat.format(new Date())));
+
+            builder.append(ResponseLogger.log(response));
+
+            System.out.print(builder.toString());
+            System.out.flush();
+        }
+        catch (Exception e)
+        {
+            // ignore
+        }
     }
 }
