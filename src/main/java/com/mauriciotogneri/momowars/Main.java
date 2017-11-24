ae7b8390036ce7ca25a68dbf5e36abd9b144daa1
mtogneri <mtogneri@openwt.com>
2017-11-24 19:53:16 +0100

    Updated libraries

--- src/main/java/com/mauriciotogneri/momowars/Main.java
+++ src/main/java/com/mauriciotogneri/momowars/Main.java
@@ -6,17 +6,19 @@ import com.mauriciotogneri.momowars.database.Database;
 public class Main extends Jerry
 {
     private final int port;
+    private final Mode mode;
     public static Database database;
 
-    public Main(int port, String databaseUrl, int connectionPoolSize) throws Exception
+    public Main(int port, Mode mode, String databaseUrl, int connectionPoolSize) throws Exception
     {
         this.port = port;
+        this.mode = mode;
         database = new Database(databaseUrl, connectionPoolSize);
     }
 
     public void start() throws Exception
     {
-        start(port, getClass().getPackage().getName());
+        start(port, mode, getClass().getPackage());
     }
 
     public static void main(String[] args) throws Exception
@@ -25,7 +27,7 @@ public class Main extends Jerry
         String databaseUrl = System.getenv("JDBC_DATABASE_URL");
         int connectionPoolSize = Integer.valueOf(System.getenv("CONNECTION_POOL_SIZE"));
 
-        Main main = new Main(port, databaseUrl, connectionPoolSize);
+        Main main = new Main(port, Mode.LOCAL, databaseUrl, connectionPoolSize);
         main.start();
     }
 }
