01953d39636d000abe34f85ceeab4b9ca8790e91
mtogneri <mtogneri@openwt.com>
2017-11-26 00:57:20 +0100

    Updated example unit test

--- src/main/java/com/mauriciotogneri/momowars/database/Database.java
+++ src/main/java/com/mauriciotogneri/momowars/database/Database.java
@@ -1,14 +1,8 @@
 package com.mauriciotogneri.momowars.database;
 
-import com.mauriciotogneri.javautils.Resource;
-import com.mauriciotogneri.momowars.database.SQL.IndexQueries;
-import com.mauriciotogneri.momowars.database.SQL.RelationshipQueries;
-import com.mauriciotogneri.momowars.database.SQL.TableQueries;
-import com.mauriciotogneri.momowars.database.SQL.TypeQueries;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
-import java.sql.Statement;
 
 public class Database
 {
@@ -31,43 +25,6 @@ public class Database
         connectionPool.setMaximumPoolSize(poolSize);
         connectionPool.setAutoCommit(false);
 
-        //initialize(connectionPool.getConnection());
-
         return connectionPool;
-    }
-
-    private void initialize(Connection connection) throws Exception
-    {
-        execute(connection, TypeQueries.CELL_TYPE);
-        execute(connection, TypeQueries.GAME_STATUS);
-        execute(connection, TypeQueries.MOVEMENT_TYPE);
-        execute(connection, TypeQueries.PLAYER_STATUS);
-        execute(connection, TypeQueries.UNIT_TYPE);
-
-        execute(connection, TableQueries.MAP);
-        execute(connection, TableQueries.CELL);
-        execute(connection, TableQueries.ACCOUNT);
-        execute(connection, TableQueries.GAME);
-        execute(connection, TableQueries.PLAYER);
-        execute(connection, TableQueries.UNIT);
-        execute(connection, TableQueries.QUEUE);
-
-        execute(connection, IndexQueries.ACCOUNT_SESSION);
-
-        execute(connection, RelationshipQueries.ACCOUNT_GAMES);
-        execute(connection, RelationshipQueries.GAME_PLAYERS);
-        execute(connection, RelationshipQueries.MAP_CELLS);
-        execute(connection, RelationshipQueries.PLAYER_QUEUES);
-        execute(connection, RelationshipQueries.PLAYER_UNITS);
-
-        connection.commit();
-    }
-
-    private void execute(Connection connection, String sqlFile) throws Exception
-    {
-        try (Statement statement = connection.createStatement())
-        {
-            statement.execute(Resource.string(sqlFile));
-        }
     }
 }
--- src/test/java/com/mauriciotogneri/momowars/BaseTest.java
+++ src/test/java/com/mauriciotogneri/momowars/BaseTest.java
@@ -1,10 +1,18 @@
 package com.mauriciotogneri.momowars;
 
+import com.mauriciotogneri.javautils.Resource;
 import com.mauriciotogneri.jerry.Jerry.Mode;
+import com.mauriciotogneri.momowars.database.SQL.IndexQueries;
+import com.mauriciotogneri.momowars.database.SQL.RelationshipQueries;
+import com.mauriciotogneri.momowars.database.SQL.TableQueries;
+import com.mauriciotogneri.momowars.database.SQL.TypeQueries;
 
 import org.eclipse.jetty.server.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class BaseTest
 {
@@ -18,8 +26,46 @@ public class BaseTest
         //int connectionPoolSize = Integer.valueOf(System.getenv("CONNECTION_POOL_SIZE"));
 
         Main main = new Main(5000, Mode.LOCAL, "jdbc:postgresql://localhost:5432/momowarstest?user=postgres&password=password", 3);
+
+        initialize(Main.database.newConnection());
+
         server = main.server();
         server.start();
+    }
+
+    private static void initialize(Connection connection) throws Exception
+    {
+        execute(connection, TypeQueries.CELL_TYPE);
+        execute(connection, TypeQueries.GAME_STATUS);
+        execute(connection, TypeQueries.MOVEMENT_TYPE);
+        execute(connection, TypeQueries.PLAYER_STATUS);
+        execute(connection, TypeQueries.UNIT_TYPE);
+
+        execute(connection, TableQueries.MAP);
+        execute(connection, TableQueries.CELL);
+        execute(connection, TableQueries.ACCOUNT);
+        execute(connection, TableQueries.GAME);
+        execute(connection, TableQueries.PLAYER);
+        execute(connection, TableQueries.UNIT);
+        execute(connection, TableQueries.QUEUE);
+
+        execute(connection, IndexQueries.ACCOUNT_SESSION);
+
+        execute(connection, RelationshipQueries.ACCOUNT_GAMES);
+        execute(connection, RelationshipQueries.GAME_PLAYERS);
+        execute(connection, RelationshipQueries.MAP_CELLS);
+        execute(connection, RelationshipQueries.PLAYER_QUEUES);
+        execute(connection, RelationshipQueries.PLAYER_UNITS);
+
+        connection.commit();
+    }
+
+    private static void execute(Connection connection, String sqlFile) throws Exception
+    {
+        try (Statement statement = connection.createStatement())
+        {
+            statement.execute(Resource.string(sqlFile));
+        }
     }
 
     @AfterClass
