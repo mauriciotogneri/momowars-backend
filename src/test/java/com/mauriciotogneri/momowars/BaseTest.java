dd7fbe8293b7207c50f538abae130060d8ba410f
mtogneri <mtogneri@openwt.com>
2017-11-26 00:39:35 +0100

    Updated example unit test

--- src/test/java/com/mauriciotogneri/momowars/BaseTest.java
+++ src/test/java/com/mauriciotogneri/momowars/BaseTest.java
@@ -17,7 +17,7 @@ public class BaseTest
         //String databaseUrl = System.getenv("JDBC_DATABASE_URL");
         //int connectionPoolSize = Integer.valueOf(System.getenv("CONNECTION_POOL_SIZE"));
 
-        Main main = new Main(5000, Mode.LOCAL, "jdbc:postgresql://localhost:5432/momowars?user=postgres&password=password", 3);
+        Main main = new Main(5000, Mode.LOCAL, "jdbc:postgresql://localhost:5432/momowarstest?user=postgres&password=password", 3);
         server = main.server();
         server.start();
     }
